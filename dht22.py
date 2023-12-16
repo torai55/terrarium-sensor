6f9d74f3c4238a32cabdf88cea8c4b80f94df9b4
Torai Lin <torailin55@gmail.com>
2023-12-16 22:20:26 +0800

    update: dht22

---
+++ dht22.py
@@ -0,0 +1,28 @@
+import time
+import board
+import adafruit_dht
+
+dht_device = adafruit_dht.DHT22(board.D6)
+
+while True:
+    try:
+        # Print the values to the serial port
+        temperature_c = dht_device.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dht_device.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dht_device.exit()
+        raise error
+
+    time.sleep(2.0)
